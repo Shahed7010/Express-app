ecebf3792cb279cee0c145ddf4c15d2c8c61be91
Shaheduzzaman Shahed <shahed@Shaheduzzamans-MacBook-Pro.local>
2023-10-08 09:55:29 +0600

    post request to add

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs = require('fs');
 
 let app = express();
+app.use(express.json());
 
 let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
 //route
@@ -15,6 +16,26 @@ app.get('/api/v1/movies', (req, res) => {
         }
     });
 })
+
+app.post('/api/v1/movies', (req, res) => {
+    // console.log(req.body)
+    const newId = movies.slice(-1)[0].id + 1;
+    const newMovie = Object.assign({id: newId}, req.body);
+
+    movies.push(newMovie);
+
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        res.status(200).json({
+            status: "success",
+            count: movies.length,
+            data: {
+                movie: newMovie
+            }
+        });
+    });
+    console.log(newId);
+})
+
 
 
 //create server
