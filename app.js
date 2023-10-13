b2f7a294790a0c6d659d6442061148707e2e5385
Shaheduzzaman Shahed <shahed@Shaheduzzamans-MacBook-Pro.local>
2023-10-13 09:35:08 +0600

    crud working

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ let app = express();
 app.use(express.json());
 
 let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
-//route
-app.get('/api/v1/movies', (req, res) => {
+
+//Methods
+const getAllMovies = (req, res) => {
     res.status(200).json({
         status: "success",
         count: movies.length,
@@ -15,11 +16,10 @@ app.get('/api/v1/movies', (req, res) => {
             movies: movies
         }
     });
-})
-
-app.post('/api/v1/movies', (req, res) => {
+}
+const storeMovie = (req, res) => {
     // console.log(req.body)
-    const newId = movies.slice(-1)[0].id + 1;
+    const newId = movies.at(-1).id + 1;
     const newMovie = Object.assign({id: newId}, req.body);
 
     movies.push(newMovie);
@@ -33,9 +33,80 @@ app.post('/api/v1/movies', (req, res) => {
             }
         });
     });
-    console.log(newId);
-})
+}
+const getMovie = (req, res) => {
+    const id = req.params.id * 1;
+    const movie = movies.find((m) => m.id === id);
 
+    if (!movie) {
+        return res.status(404).json({
+            status: "fail",
+            data: {
+                message: `No movie found with id ${id}`
+            }
+        });
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            movie: movie
+        }
+    });
+}
+const updateMovie = (req, res) => {
+    const id = req.params.id * 1;
+    const movie = movies.find((m) => m.id === id);
+    const index = movies.indexOf(movie);
+    if (!movie) {
+        return res.status(404).json({
+            status: "fail",
+            data: {
+                message: `No movie found with id ${id}`
+            }
+        });
+    }
+    Object.assign(movie, req.body);
+    movies[index] = movie;
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        res.status(200).json({
+            status: "success",
+            count: movies.length,
+            data: {
+                movie: movie
+            }
+        });
+    });
+}
+const deleteMovie = (req, res) => {
+    const id = req.params.id * 1;
+    const movie = movies.find((m) => m.id === id);
+    const index = movies.indexOf(movie);
+    if (!movie) {
+        return res.status(404).json({
+            status: "fail",
+            data: {
+                message: `No movie found with id ${id}`
+            }
+        });
+    }
+    movies.splice(index, 1);
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        res.status(204).json({
+            status: "success",
+            count: movies.length,
+            data: {
+                movie: null
+            }
+        });
+    });
+}
+//route
+app.get('/api/v1/movies', getAllMovies);
+app.post('/api/v1/movies', storeMovie);
+app.get('/api/v1/movies/:id', getMovie);
+app.patch('/api/v1/movies/:id', updateMovie);
+app.delete('/api/v1/movies/:id', deleteMovie);
 
 
 //create server
