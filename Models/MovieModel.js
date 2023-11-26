e69977232e83e117524a174f6376f036e0bf9209
Shaheduzzaman Shahed <shahed@Shaheduzzamans-MacBook-Pro.local>
2023-11-26 20:33:22 +0600

    dummy movie data import functionality added

--- Models/MovieModel.js
+++ Models/MovieModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const {now} = require("mongoose");
 
 
 const movieSchema = new mongoose.Schema({
@@ -7,17 +8,55 @@ const movieSchema = new mongoose.Schema({
         required: [true, 'Name field is required.'],
         unique: true
     },
-    description: String,
+    description: {
+        type: String,
+        required: [true, 'Description field is required.'],
+        trim: true
+    },
     duration: {
         type: Number,
         required: [true, 'Duration field is required.'],
     },
     ratings: {
         type: Number,
-        default: 0
-    }
+    },
+    totalRatings: {
+        type: Number,
+    },
+    releaseYear: {
+        type: Number,
+        required: [true, 'Release Year field is required.'],
+    },
+    releaseDate: {
+        type: Date,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    genres: {
+        type: [String],
+        required: [true, 'Genres field is required.'],
+    },
+    directors: {
+        type: [String],
+        required: [true, 'Directors field is required.'],
+    },
+    coverImage: {
+        type: [String],
+        required: [true, 'Cover Image field is required.'],
+    },
+    actors: {
+        type: [String],
+        required: [true, 'Cover Image field is required.'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price field is required.'],
+    },
+
 });
 
-const Movie = mongoose.model('movies',movieSchema);
+const Movie = mongoose.model('movies', movieSchema);
 
 module.exports = Movie;
